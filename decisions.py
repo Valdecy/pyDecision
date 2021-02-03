d2f5405770d7a77cf5aee846c87fcb09340ac6c6
Valdecy Pereira <valdecypereira@yahoo.com.br>
2021-02-03 16:08:44 -0300

    Update decisions.py

--- decisions.py
+++ decisions.py
@@ -16,7 +16,10 @@ from py_decisions.dematel.dematel   import dematel_method
 from py_decisions.dematel.fuzzy_dematel import fuzzy_dematel_method
 
 # EDAS
-from py_decisions.edas.edas        import edas_method
+from py_decisions.edas.edas         import edas_method
+
+# Fuzzy EDAS
+from py_decisions.edas.fuzzy_edas   import fuzzy_edas_method
 
 # ELECTRE
 from py_decisions.electre.e_i       import electre_i
@@ -141,7 +144,30 @@ dataset = np.array([
 # Call EDAS Function  
 rank = edas_method(dataset, criterion_type, weights, graph = True)
 
-###############################################################################
+##############################################################################
+
+# Fuzzy EDAS
+ 
+# Weigths
+weights = list([
+          [ (  0.1,   0.2,   0.3), (  0.7,   0.8,   0.9), (  0.3,   0.5,   0.8) ]    
+    ])
+
+# Load Criterion Type: 'max' or 'min'
+criterion_type = ['max', 'max', 'min']
+
+# Dataset
+dataset = list([
+    [ (  3,   6,   9), (  5,   8,   9), (  5,   7,   9) ],   #a1
+    [ (  5,   7,   9), (  3,   7,   9), (  3,   5,   7) ],   #a2
+    [ (  5,   8,   9), (  3,   5,   7), (  1,   2,   3) ],   #a3
+    [ (  1,   2,   4), (  1,   4,   7), (  1,   2,   5) ]    #a4
+    ])
+
+# Call Fuzzy EDAS
+fuzzy_rank, rank = fuzzy_edas_method(dataset, weights, criterion_type, graph = True)
+
+##############################################################################
 
 # ELECTRE I
     
