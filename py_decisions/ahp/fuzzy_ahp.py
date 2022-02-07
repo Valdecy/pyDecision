8ef35c46f288184dd55e9500e100782e860cf982
Valdecy Pereira <valdecypereira@yahoo.com.br>
2022-02-06 21:12:37 -0300

    Update fuzzy_ahp.py

--- py_decisions/ahp/fuzzy_ahp.py
+++ py_decisions/ahp/fuzzy_ahp.py
@@ -1,6 +1,7 @@
 ###############################################################################
 
 # Required Libraries
+import numpy as np
 
 ###############################################################################
 
@@ -10,6 +11,10 @@ def fuzzy_ahp_method(dataset):
     s_row   = []
     f_w     = []
     d_w     = []
+    inc_rat = np.array([0, 0, 0, 0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.45])
+    X       = [(item[0] + 4*item[1] + item[2])/6 for i in range(0, len(dataset)) for item in dataset[i] ]
+    X       = np.asarray(X)
+    X       = np.reshape(X, (len(dataset), len(dataset)))
     for i in range(0, len(dataset)):
         a, b, c = 1, 1, 1
         for j in range(0, len(dataset[i])):
@@ -29,10 +34,11 @@ def fuzzy_ahp_method(dataset):
         a, b, c = a*(U**-1), b*(M**-1), c*(L**-1)
         f_w.append( ( a, b, c ) )
         d_w.append( (a + b + c)/3 )
-    n_w = [item/sum(d_w) for item in d_w]  
-    return f_w, d_w, n_w
+    n_w      = [item/sum(d_w) for item in d_w]
+    vector   = np.sum(X*n_w, axis = 1)/n_w
+    lamb_max = np.mean(vector)
+    cons_ind = (lamb_max - X.shape[1])/(X.shape[1] - 1)
+    rc       = cons_ind/inc_rat[X.shape[1]]
+    return f_w, d_w, n_w, rc
 
 ###############################################################################
-    
-       
-    
