3b40562b5cd60f5518f2be8e587f2f301dbaea71
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-07-25 18:36:39 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pyDecision',
-    version='3.0.3',
+    version='4.0.3',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
@@ -15,6 +15,8 @@ setup(
     install_requires=[
         'matplotlib',
         'numpy',
+        'openai',
+		'pandas',
         'scikit-learn',
         'scipy'
     ],
