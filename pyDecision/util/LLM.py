cd20c938268ec27f61f34cbf2715c55f06ee6b9e
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-07-29 21:38:19 -0300

    Update LLM.py

--- pyDecision/util/LLM.py
+++ pyDecision/util/LLM.py
@@ -2,21 +2,31 @@
 
 # Required Libraries
 import openai  
+a=openai.Model.list()
 
 ###############################################################################
 
 # Function: Ask GPT Rank
 def ask_chatgpt_rank(ranks, char_limit = 4097, api_key = 'your_api_key_here', query = 'which methods are more similar?', model = 'text-davinci-003', max_tokens = 2000, n = 1, temperature = 0.8):
     def query_chatgpt(prompt, model = model, max_tokens = max_tokens, n = n, temperature = temperature):
-        response = openai.Completion.create(
-                                            engine      = model,
-                                            prompt      = prompt,
-                                            max_tokens  = max_tokens,
-                                            n           = n,
-                                            stop        = None,
-                                            temperature = temperature
-                                            )
-        return response.choices[0].text.strip()
+        try: 
+            response = openai.ChatCompletion.create(
+                                                    model      = model,
+                                                    messages   = [{'role': 'user', 'content': prompt}],
+                                                    max_tokens = max_tokens
+                                                    )
+            response = response['choices'][0]['message']['content']
+        except:
+            response = openai.Completion.create(
+                                                engine      = model,
+                                                prompt      = prompt,
+                                                max_tokens  = max_tokens,
+                                                n           = n,
+                                                stop        = None,
+                                                temperature = temperature
+                                                )
+            response = response.choices[0].text.strip()
+        return response
     corpus         = ranks.to_string(index = False)
     openai.api_key = api_key
     context        = ' knowing that for the given outranking table, the columns represent MCDA methods and the rows alternatives. Each cell indicates the rank of the alternatives (1 = 1st position, 2 = 2nd position, and so on) '
@@ -29,15 +39,24 @@ def ask_chatgpt_rank(ranks, char_limit = 4097, api_key = 'your_api_key_here', qu
 # Function: Ask GPT Weights
 def ask_chatgpt_weights(weights, char_limit = 4097, api_key = 'your_api_key_here', query = 'which methods are more similar?', model = 'text-davinci-003', max_tokens = 2000, n = 1, temperature = 0.8):
     def query_chatgpt(prompt, model = model, max_tokens = max_tokens, n = n, temperature = temperature):
-        response = openai.Completion.create(
-                                            engine      = model,
-                                            prompt      = prompt,
-                                            max_tokens  = max_tokens,
-                                            n           = n,
-                                            stop        = None,
-                                            temperature = temperature
-                                            )
-        return response.choices[0].text.strip()
+        try: 
+            response = openai.ChatCompletion.create(
+                                                    model      = model,
+                                                    messages   = [{'role': 'user', 'content': prompt}],
+                                                    max_tokens = max_tokens
+                                                    )
+            response = response['choices'][0]['message']['content']
+        except:
+            response = openai.Completion.create(
+                                                engine      = model,
+                                                prompt      = prompt,
+                                                max_tokens  = max_tokens,
+                                                n           = n,
+                                                stop        = None,
+                                                temperature = temperature
+                                                )
+            response = response.choices[0].text.strip()
+        return response
     corpus         = weights.to_string(index = False)
     openai.api_key = api_key
     context        = ' knowing that for the given table, the columns represent MCDA methods and the rows criterion. Each cell indicates the weight of each criterion (the higher the value, the most important is the criterion) calculate by each MCDA method'
