78501ebc1dd4bdb739e61303e60e72dd24f3778c
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-08-02 17:42:43 -0300

    Update bwm.py

--- pyDecision/algorithm/bwm.py
+++ pyDecision/algorithm/bwm.py
@@ -3,19 +3,21 @@
 # Required Libraries
 import numpy as np
 
-from pyDecision.util.gwo import grey_wolf_optimizer
+from scipy.optimize import minimize, Bounds
 
 ###############################################################################
 
 # Function: BWM
-def bw_method(dataset, mic, lic, size = 50, iterations = 150, verbose = True):
+def bw_method(dataset, mic, lic, verbose = True):
     X         = np.copy(dataset)/1.0
     best      = np.where(mic == 1)[0][0]
     worst     = np.where(lic == 1)[0][0]
     pairs_b   = [(best, i)  for i in range(0, mic.shape[0])]
     pairs_w   = [(i, worst) for i in range(0, mic.shape[0]) if (i, worst) not in pairs_b]
+    
+    ################################################
     def target_function(variables):
-        eps       = [float('+inf')]
+        eps = [float('+inf')]
         for pair in pairs_b:
             i, j = pair
             diff = abs(variables[i] - variables[j]*mic[j])
@@ -31,8 +33,13 @@ def bw_method(dataset, mic, lic, size = 50, iterations = 150, verbose = True):
         else:
             eps = max(eps[1:])
         return eps
-    weights = grey_wolf_optimizer(pack_size = size, min_values = [0.01]*X.shape[1], max_values = [1]*X.shape[1], iterations = iterations, target_function = target_function, verbose = verbose)
-    weights = weights[0][:-1]/sum(weights[0][:-1])
+    ################################################
+    
+    variables = np.ones(X.shape[1])
+    bounds    = Bounds([0.0000001]*len(variables), [1]*len(variables))
+    results   = minimize(target_function, variables, method = 'L-BFGS-B', bounds = bounds)
+    weights   = results.x
+    weights   = weights/np.sum(weights)
     return weights
 
 ###############################################################################
