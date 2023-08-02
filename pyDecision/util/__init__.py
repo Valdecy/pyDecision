4923de310ea56b17a18b79521178d663323c6baa
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-08-02 17:39:05 -0300

    Update __init__.py

--- pyDecision/util/__init__.py
+++ pyDecision/util/__init__.py
@@ -1,3 +1,2 @@
-from .ga      import genetic_algorithm
-from .gwo     import grey_wolf_optimizer
-from .LLM     import ask_chatgpt_rank, ask_chatgpt_weights
+from .ga  import genetic_algorithm
+from .LLM import ask_chatgpt_corr, ask_chatgpt_rank, ask_chatgpt_weights 
