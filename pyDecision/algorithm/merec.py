3d4ffd19ad8419b44688d4b0f3820b06bececad7
Valdecy Pereira <valdecypereira@yahoo.com.br>
2023-10-26 20:46:45 -0300

    Update merec.py

--- pyDecision/algorithm/merec.py
+++ pyDecision/algorithm/merec.py
@@ -10,9 +10,15 @@ def merec_method(dataset, criterion_type):
     X = np.copy(dataset)/1.0
     for j in range(0, X.shape[1]):
         if (criterion_type[j] == 'max'):
-            X[:,j] = np.min(X[:,j]) / X[:,j]
+            divisor = np.min(X[:,j])
+            if (divisor == 0):
+                divisor =  1e-9
+            X[:,j] = divisor / (X[:,j] + 1e-9)
         else:
-            X[:,j] = X[:,j] / np.max(X[:,j])
+            divisor = np.max(X[:,j])
+            if (divisor == 0):
+                divisor =  1e-9
+            X[:,j] = X[:,j] / (divisor + 1e-9)
     S = np.log(1 + (1/X.shape[1] * np.sum(np.abs(np.log(X)), axis = 1)))
     R = np.zeros(X.shape)
     for j in range(0, X.shape[1]):
