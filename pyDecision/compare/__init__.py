71fb89e7293134632b599f482f35fb5834d32fe7
Valdecy Pereira <valdecypereira@yahoo.com.br>
2024-04-26 23:03:36 -0300

    Update __init__.py

--- pyDecision/compare/__init__.py
+++ pyDecision/compare/__init__.py
@@ -1,1 +1,1 @@
-from .compare import compare_ranks_crisp, compare_ranks_fuzzy, compare_weights, plot_rank_freq, corr_viz
+from .compare import compare_ranks_crisp, compare_ranks_fuzzy, compare_weights, compare_weights_fuzzy, plot_rank_freq, corr_viz
