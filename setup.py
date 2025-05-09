5b62385ab2239214379fbf16559992556c24befd
Valdecy Pereira <valdecypereira@yahoo.com.br>
2025-05-09 12:31:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pydecision',
-    version='4.7.5',
+    version='4.7.7',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
