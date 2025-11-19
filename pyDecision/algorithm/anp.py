656b224e608f05083efdb6dcab564af7565535f1
Valdecy Pereira <valdecypereira@yahoo.com.br>
2025-11-19 09:23:26 -0300

    Add files via upload

---
+++ pyDecision/algorithm/anp.py
@@ -0,0 +1,19 @@
+###############################################################################
+
+# Required Libraries
+import numpy as np
+
+###############################################################################
+
+# Function: AHP
+def anp_method(W, max_iter = 100, tol = 1e-12):
+    M_prev = W.copy()
+    M      = W.copy()
+    for k in range(0, max_iter):
+        M = M @ W
+        if np.allclose(M, M_prev, atol = tol):
+            return M
+        M_prev = M
+    return M
+
+###############################################################################
