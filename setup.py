b68f28be888581e861ea334348d0da26c036941f
Valdecy Pereira <valdecypereira@yahoo.com.br>
2025-11-21 23:22:04 -0300

    Bump version from 4.8.2 to 4.8.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name='pydecision',
-    version='4.8.2',
+    version='4.8.5',
     license='GNU',
     author='Valdecy Pereira',
     author_email='valdecy.pereira@gmail.com',
